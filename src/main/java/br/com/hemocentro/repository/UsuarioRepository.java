d01714a64e66247de95c0cb6f5875e213ad87c43
Leonardo Holanda <leoholanda23@gmail.com>
2016-08-02 11:43:44 -0400

    Removendo UsuarioRepository

--- src/main/java/br/com/hemocentro/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.hemocentro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.hemocentro.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
